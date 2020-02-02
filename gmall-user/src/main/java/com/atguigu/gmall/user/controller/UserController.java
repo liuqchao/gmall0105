dc2554395fdbc9ccb5501aee075403c73a14f386
qingchao.liu <qingchao.liu@matrixport.com>
2020-02-02 10:17:54 +0800

    gmall-api模块添加完毕

--- gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
+++ gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -1,8 +1,8 @@
 package com.atguigu.gmall.user.controller;
 
-import com.atguigu.gmall.user.bean.UmsMember;
-import com.atguigu.gmall.user.bean.UmsMemberReceiveAddress;
-import com.atguigu.gmall.user.service.UserService;
+import com.atguigu.gmall.service.UserService;
+import com.atguigu.gmall.bean.UmsMember;
+import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +16,6 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @RequestMapping("getReceiveAddressByMemberId")
-    @ResponseBody
-    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId){
-        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = userService.getReceiveAddressByMemberId(memberId);
-        return umsMemberReceiveAddresses;
-    }
 
 
     @RequestMapping("getAllUser")
